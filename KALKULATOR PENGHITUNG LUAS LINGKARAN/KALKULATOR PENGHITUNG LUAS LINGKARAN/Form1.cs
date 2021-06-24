85e0e5c246db0750e812be19c3c57913631cd08d
arijuntak <86229500+arijuntak@users.noreply.github.com>
2021-06-24 09:41:51 +0700

    Add files via upload
    upload file

---
+++ KALKULATOR PENGHITUNG LUAS LINGKARAN/KALKULATOR PENGHITUNG LUAS LINGKARAN/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KALKULATOR_PENGHITUNG_LUAS_LINGKARAN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double phi = 3.14, jari, hasil;
+            jari = double.Parse(this.textBox1.Text);
+            hasil = phi * jari * jari;
+            this.textBox2.Text = hasil.ToString();
+        }
+    }
+}
